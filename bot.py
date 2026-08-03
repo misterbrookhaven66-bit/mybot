bc74cecc3b81591ec1707279ee55f96d652d15a2
misterbrookhaven66-bit <misterbrookhaven66@gmail.com>
2026-08-03 11:23:23 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = os.environ["TOKEN"]
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет! Бот работает.")
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
